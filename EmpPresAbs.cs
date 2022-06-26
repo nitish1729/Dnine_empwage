4f9b4e36036a9839f8a3273c658cb0f398d1feb5
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-26 21:59:51 +0530

    EmpPresAbs

---
+++ EmpPresAbs.cs
@@ -0,0 +1,31 @@
+﻿public class Employee
+{
+    int WagePerHrs = 20;
+    int FullTime = 8;
+    int NoWork = 0;
+
+    //void display()
+    Employee()
+
+    {
+        Random ran = new Random();
+
+        int AttenCheck = ran.Next(1, 10);
+
+        if (AttenCheck == FullTime)
+        {
+            Console.WriteLine("Emp is Present");
+            Console.WriteLine("EmpWage : " + (WagePerHrs * FullTime));
+        }
+        else
+        {
+            Console.WriteLine("Emp is Absent");
+            Console.WriteLine("EmpWage : " + (WagePerHrs * NoWork));
+        }
+    }
+    public static void Main()
+    {
+        Employee wages = new Employee();
+        // wages.display();
+    }
+}
