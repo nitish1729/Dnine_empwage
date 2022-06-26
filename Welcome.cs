5a5f0a09f46cbfce6b2094895aa6074a4fd1b70c
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-26 21:53:13 +0530

    WelLcome

---
+++ Welcome.cs
@@ -0,0 +1,19 @@
+﻿
+public class Wellcome 
+{
+    
+public void display()
+    {
+        Console.WriteLine("Welcome to Employee Wage Computation");
+    }
+
+    public static void Main()
+    {
+        Wellcome show = new Wellcome();
+        show.display();
+    }
+}
+
+
+
+
